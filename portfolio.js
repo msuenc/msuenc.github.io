87aade501b27dc2a0325f6185ebf1733365bc0e4
Michelle Sue <m.sue@cci-formation.nc>
2020-09-16 11:04:11 +1100

    update

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,5 @@
 
 
-
-
-//$('#t1').dblclick(function(){$('#cache1').removeAttr('hidden');});
 //$('#t2').dblclick(function(){$('#cache2').removeAttr('hidden');});
 //$('#t3').dblclick(function(){$('#langues').removeAttr('hidden');$('#informatiques').removeAttr('hidden');});
 //$('#t4').dblclick(function(){$('#arts').removeAttr('hidden');});
