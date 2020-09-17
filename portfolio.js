07f7165a48249f2f6964fb255d1939d832869dac
Michelle Sue <m.sue@cci-formation.nc>
2020-09-17 22:55:15 +1100

    update

--- portfolio.js
+++ portfolio.js
@@ -4,6 +4,8 @@ $('#t1').click(function(){
     $('#langues').toggle();
     $('#informatiques').toggle();
 
+    $('#box1').css('height', 'unset');
+
     $('#box2').toggle();
     $('#box3').toggle();
     $('#box4').toggle();
@@ -13,6 +15,8 @@ $('#t1').click(function(){
 $('#t2').click(function(){
     $('#etudes').toggle();
     $('#experiences').toggle();
+
+    $('#box2').css('height', 'unset');
 
     $('#box1').toggle();
     $('#box3').toggle();
@@ -24,6 +28,8 @@ $('#t3').click(function(){
     $('#arts').toggle();
     $('#sports').toggle();
 
+    $('#box3').css('height', 'unset');
+
     $('#box1').toggle();
     $('#box2').toggle();
     $('#box4').toggle();
@@ -33,10 +39,11 @@ $('#t3').click(function(){
 $('#t4').click(function(){
     $('#contacts').toggle();
 
+    $('#box4').css('height', 'unset');
+
     $('#box1').toggle();
     $('#box2').toggle();
     $('#box3').toggle();
     $('.arbre').toggle();
 });
 
-//Changer les images$('#coiffure').mouseover(function(){$('#coiffure').attr('src' , 'images/');});
