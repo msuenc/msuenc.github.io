357e196af4361cec7473bc67d066019fdd694334
Michelle Sue <m.sue@cci-formation.nc>
2020-10-06 13:34:13 +1100

    Update

--- portfolio.js
+++ portfolio.js
@@ -1,31 +1,31 @@
 //Afficher et cacher les parties
-$('#box1').click(function(){
-    $('#presentation').toggle();
-    $('#langues').toggle();
-    $('#informatiques').toggle();
-    $('#btn1').toggle();
+$('#box1').click(function() {
+    $('#presentation').show();
+    $('#langues').show();
+    $('#informatiques').show();
+    $('#btn1').show();
 
     $('#t1').css('position' , 'unset');
     $('#box1').css('height', 'unset');
 
-    $('#box2').toggle();
-    $('#box3').toggle();
-    $('#box4').toggle();
-    $('.arbre').toggle();
-    $('.snowflakes').toggle();
+    $('#box2').hide();
+    $('#box3').hide();
+    $('#box4').hide();
+    $('.arbre').hide();
+    $('.snowflakes').hide();
 });
 
 $('#btn1').click(function(){
-    $('#presentation').toggle();
-    $('#langues').toggle();
-    $('#informatiques').toggle();
-    $('#btn1').toggle();
+    $('#presentation').hide();
+    $('#langues').hide();
+    $('#informatiques').hide();
+    $('#btn1').hide();
 
-    $('#box2').toggle();
-    $('#box3').toggle();
-    $('#box4').toggle();
-    $('.arbre').toggle();
-    $('.snowflakes').toggle();
+    $('#box2').show();
+    $('#box3').show();
+    $('#box4').show();
+    $('.arbre').show();
+    $('.snowflakes').show();
 
     $('#t1').css('position' , 'absolute');
 
@@ -38,30 +38,30 @@ $('#btn1').click(function(){
 
 
 $('#box2').click(function(){
-    $('#etudes').toggle();
-    $('#experiences').toggle();
-    $('#btn2').toggle();
+    $('#etudes').show();
+    $('#experiences').show();
+    $('#btn2').show();
 
     $('#t2').css('position' , 'unset');
     $('#box2').css('height', 'unset');
 
-    $('#box1').toggle();
-    $('#box3').toggle();
-    $('#box4').toggle();
-    $('.arbre').toggle();
-    $('.flowers').toggle();
+    $('#box1').hide();
+    $('#box3').hide();
+    $('#box4').hide();
+    $('.arbre').hide();
+    $('.flowers').hide();
 });
 
 $('#btn2').click(function(){
-    $('#etudes').toggle();
-    $('#experiences').toggle();
-    $('#btn2').toggle();
+    $('#etudes').hide();
+    $('#experiences').hide();
+    $('#btn2').hide();
 
-    $('#box1').toggle();
-    $('#box3').toggle();
-    $('#box4').toggle();
-    $('.arbre').toggle();
-    $('.flowers').toggle();
+    $('#box1').show();
+    $('#box3').show();
+    $('#box4').show();
+    $('.arbre').show();
+    $('.flowers').show();
 
     $('#t2').css('position' , 'absolute');
 
@@ -74,30 +74,30 @@ $('#btn2').click(function(){
 
 
 $('#box3').click(function(){
-    $('#arts').toggle();
-    $('#sports').toggle();
-    $('#btn3').toggle();
+    $('#arts').show();
+    $('#sports').show();
+    $('#btn3').show();
 
     $('#t3').css('position' , 'unset');
     $('#box3').css('height', 'unset');
 
-    $('#box1').toggle();
-    $('#box2').toggle();
-    $('#box4').toggle();
-    $('.arbre').toggle();
-    $('.deadleafs').toggle();
+    $('#box1').hide();
+    $('#box2').hide();
+    $('#box4').hide();
+    $('.arbre').hide();
+    $('.deadleafs').hide();
 });
 
 $('#btn3').click(function(){
-    $('#arts').toggle();
-    $('#sports').toggle();
-    $('#btn3').toggle();
+    $('#arts').hide();
+    $('#sports').hide();
+    $('#btn3').hide();
 
-    $('#box1').toggle();
-    $('#box2').toggle();
-    $('#box4').toggle();
-    $('.arbre').toggle();
-    $('.deadleafs').toggle();
+    $('#box1').show();
+    $('#box2').show();
+    $('#box4').show();
+    $('.arbre').show();
+    $('.deadleafs').show();
 
     $('#t3').css('position' , 'absolute');
 
@@ -110,28 +110,28 @@ $('#btn3').click(function(){
 
 
 $('#box4').click(function(){
-    $('#contacts').toggle();
-    $('#btn4').toggle();
-    $('.leafs').toggle();
+    $('#contacts').show();
+    $('#btn4').show();
 
     $('#t4').css('position' , 'unset');
     $('#box4').css('height', 'unset');
 
-    $('#box1').toggle();
-    $('#box2').toggle();
-    $('#box3').toggle();
-    $('.arbre').toggle();
+    $('#box1').hide();
+    $('#box2').hide();
+    $('#box3').hide();
+    $('.arbre').hide();
+    $('.leafs').hide();
 });
 
 $('#btn4').click(function(){
-    $('#contacts').toggle();
-    $('#btn4').toggle();
+    $('#contacts').hide();
+    $('#btn4').hide();
 
-    $('#box1').toggle();
-    $('#box2').toggle();
-    $('#box3').toggle();
-    $('.arbre').toggle();
-    $('.leafs').toggle();
+    $('#box1').show();
+    $('#box2').show();
+    $('#box3').show();
+    $('.arbre').show();
+    $('.leafs').show();
 
     $('#t4').css('position' , 'absolute');
 
